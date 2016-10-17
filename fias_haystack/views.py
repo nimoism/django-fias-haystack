8d13aa8db3c29902ab8cce89795a929659f579d1
Dmiry Puhov <dmitry.puhov@gmail.com>
2016-10-17 23:50:18 +0300

    Add filters for addresses for search

--- fias_haystack/views.py
+++ fias_haystack/views.py
@@ -1,8 +1,13 @@
+# coding=utf-8
+
 from __future__ import absolute_import, unicode_literals
 
 import operator
+from string import strip
+
 import six
 from django.conf import settings
+from django.core.validators import EMPTY_VALUES
 from django.http.response import JsonResponse
 from django.views.generic.list import BaseListView
 from fias.models.addrobj import AddrObj
@@ -15,6 +20,14 @@ class HaystackResponseView(BaseListView):
         term = request.GET.get('term', '').strip()
         if len(term) > 0:
             sqs = SearchQuerySet().using(settings.FIAS_HAYSTACK_CONNECTION_ALIAS).models(AddrObj)
+            aolevels = request.GET.get('aolevels', '')
+            aolevels = map(int, filter(lambda s: s not in EMPTY_VALUES, map(strip, aolevels.split(','))))
+            if aolevels:
+                sqs = sqs.filter(aolevel__in=aolevels)
+            socrs = request.GET.get('socrs', '')
+            socrs = map(strip, socrs.split(','))
+            if socrs:
+                sqs = sqs.filter(shortname__in=socrs)
             cleaned_words = [sqs.query.clean(word.strip()) for word in term.split(' ')]
             query_bits = [SQ(text_auto=word) for word in cleaned_words if word]
             if query_bits:
